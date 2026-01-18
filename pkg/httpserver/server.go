c818571670cb40801318410245aff4c04a45c194
Messi Lionel <gleb.yurov.1998@mail.ru>
2026-01-18 22:24:43 +0300

    add base httpserver config

---
+++ pkg/httpserver/server.go
@@ -0,0 +1,70 @@
+package httpserver
+
+import (
+	"context"
+	"net/http"
+	"runtime"
+	"time"
+	"github.com/gin-gonic/gin"
+
+)
+
+const (
+	_defautlAddr            = ":80"
+	_defaultReadTimout      = 5 * time.Second
+	_defaultWriteTimout     = 5 * time.Second
+	_defaultShutdownTimeout = 3 * time.Second
+)
+
+type Server struct {
+	HTTPServer *http.Server
+	notify     chan error
+
+	address         string
+	prefork         bool
+	readTimeout     time.Duration
+	writeTimeout    time.Duration
+	shutdownTimeout time.Duration
+}
+
+func New(opts ...Option) *Server {
+	s := &Server{
+		HTTPServer:      nil,
+		notify:          make(chan error, 1),
+		address:         _defautlAddr,
+		readTimeout:     _defaultReadTimout,
+		writeTimeout:    _defaultWriteTimout,
+		shutdownTimeout: _defaultShutdownTimeout,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+	httpserv := &http.Server{
+		Addr:         s.address,
+		WriteTimeout: s.writeTimeout,
+		ReadTimeout:  s.readTimeout,
+	}
+	s.HTTPServer = httpserv
+	if s.prefork {
+		runtime.GOMAXPROCS(1)
+	}
+	return s
+}
+
+func (s *Server) Start() {
+	go func() {
+		s.notify <- s.HTTPServer.ListenAndServe()
+		close(s.notify)
+	}()
+}
+
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+func (s *Server) Shutdown() error {
+	context, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+	return s.HTTPServer.Shutdown(context)
+}
