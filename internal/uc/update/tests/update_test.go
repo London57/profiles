7dab2b7a257a271c37b206b88a662d6a92140e82
Messi Lionel <gleb.yurov.1998@mail.ru>
2026-01-31 22:58:51 +0300

    add uc update tests

--- internal/uc/update/tests/update_test.go
+++ internal/uc/update/tests/update_test.go
@@ -2,10 +2,10 @@ package tests
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
-	"github.com/London57/profiles/internal/config"
 	"github.com/London57/profiles/internal/data/entities"
 	repo "github.com/London57/profiles/internal/interfaces/repo/mocks"
 	"github.com/London57/profiles/internal/presentation/api/http/dtos/request"
@@ -16,7 +16,7 @@ import (
 )
 
 
-func Ptr[t any](v t) *t {
+func Ptr[T any](v T) *T {
 	return &v
 }
 
@@ -32,7 +32,7 @@ func TestUpdate(t *testing.T) {
 		{
 			name: "success",
 			req: request.ProfileUpdateRequest{
-				Latitude:  14.01,
+				Latitude:  14.02,
 				Longitude: 16,
 				Username:  "123",
 				Birthday:  time.Date(1998, 6, 13, 3, 2, 0, 0, time.UTC),
@@ -48,10 +48,34 @@ func TestUpdate(t *testing.T) {
 			setup: func(ctrl *gomock.Controller) (*repo.MockProfilesRepo) {
 				repo := repo.NewMockProfilesRepo(ctrl)
 
-				repo.EXPECT().CreateProfile(gomock.Any(), gomock.Any()).Return(&entities.ProfileEntity{}, nil)
+				repo.EXPECT().UpdateProfile(gomock.Any(), gomock.Any()).Return(&entities.ProfileEntity{
+					Latitude:  14.02,
+					Longitude: 16,
+					Username:  "123",
+					Birthday:  time.Date(1998, 6, 13, 3, 2, 0, 0, time.UTC),
+					Name:      "123sdfs",
+				}, nil)
 				return repo
 			},
 			expected_err: nil,
+		},
+		{
+			name: "error",
+			req: request.ProfileUpdateRequest{
+				Latitude:  14.02,
+				Longitude: 16,
+				Username:  "123",
+				Birthday:  time.Date(1998, 6, 13, 3, 2, 0, 0, time.UTC),
+				Name:      "123sdfs",
+			},
+			expected_resp: response.ProfileUpdateResponse{},
+			setup: func(ctrl *gomock.Controller) (*repo.MockProfilesRepo) {
+				repo := repo.NewMockProfilesRepo(ctrl)
+
+				repo.EXPECT().UpdateProfile(gomock.Any(), gomock.Any()).Return(nil, errors.New("some error"))
+				return repo
+			},
+			expected_err: errors.New("some error"),
 		},
 	}
 
@@ -61,7 +85,7 @@ func TestUpdate(t *testing.T) {
 			defer ctrl.Finish()
 
 			repo := tc.setup(ctrl)
-			uc := interactors.ProfileUpdate{}.(repo, config.JWT{}, jwt)
+			uc := interactors.ProfileUpdate{}.New(repo)
 
 			ctx := context.Background()
 			res, err := uc.Exec(ctx, tc.req)
